6b9193c9415016034f9638178d1bcae11381498f
aammundi <aammundi>
2013-09-04 03:33:22 +0000

    changed wizUserID to wizCardID in processAcceptFlickedCard
    git-svn-id: svn+ssh://totastyle.com/usr/local/svn/repos/WizCard/trunk@293 3421baa5-6d19-45bd-9d2d-b653c2543eeb

--- wizserver/views.py
+++ wizserver/views.py
@@ -553,10 +553,8 @@ class ParseMsgAndDispatch:
         #To Do. if app returns the connection id cookie sent by server
         #we'd just need to lookup connection from there
         try:
-            sender = User.objects.get(id=self.sender['wizUserID'])
-            receiver = User.objects.get(id=self.receiver['wizUserID'])
-            wizcard1 = sender.wizcard
-            wizcard2 = receiver.wizcard
+            wizcard1 = Wizcard.objects.get(id=sender['wizCardID']
+            wizcard2 = Wizcard.objects.get(id=receiver['wizCardID']
         except ObjectDoesNotExist:
             self.response.error_response(errno=1, errorStr="Object does not exist")
             return self.response.response
