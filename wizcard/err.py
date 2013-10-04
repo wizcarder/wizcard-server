ac0a2dbe855ca0e1e9e4f24adb8d5ec169a04003
aammundi <aammundi>
2013-10-04 21:53:33 +0000

    creation
    git-svn-id: svn+ssh://totastyle.com/usr/local/svn/repos/WizCard/trunk@312 3421baa5-6d19-45bd-9d2d-b653c2543eeb

---
+++ wizcard/err.py
@@ -0,0 +1,13 @@
+NONE_FOUND = {errno=1, str="no results found"}
+EXISTING_MEMBER = {errno=1, str="already joined table"}
+INVALID_USERNAME_PASSWORD = {errno=1, str="invalid username or password"}
+AUTHENTICATION_FAILED = {errno=2, str="authentication failed"}
+USER_DOESNT_EXIST = {errno=3, str="user doesn't exist"}
+OBJECT_DOESNT_EXIST = {errno=3, str="object doesn't exist"}
+LOCATION_UNKNOWN = {errno=3, str="location error"}
+INTERNAL_ERROR = {errno=4, str="internal error"}
+EXISTING_CONNECTION = {errno=5, str="already connected"}
+INVALID_MESSAGE = {errno=5, str="invalid message"}
+NOT_AUTHORIZED = {errno=6, str"user is not the creator"}
+
+
