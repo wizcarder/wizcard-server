6cebd31405bce6f16e49745a849bde7c8c9ee7e1
aammundi <aammundi>
2014-06-12 23:16:24 +0000

    local run script
    git-svn-id: svn+ssh://totastyle.com/usr/local/svn/repos/WizCard/trunk@474 3421baa5-6d19-45bd-9d2d-b653c2543eeb

---
+++ run.local.sh
@@ -0,0 +1,6 @@
+ps -ef | grep "runserver" | awk '{print $2}' | xargs kill
+cat twistd.pid | xargs kill
+twistd -r select web --class=pyapns.server.APNSServer --port=7077
+sleep 2
+python manage.py runserver 192.168.0.11:8000
+
