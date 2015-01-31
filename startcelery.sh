65860af5140aa5be0e5958364845be662aacc6a8
Anand Ramani <anandramani98@gmail.com>
2015-01-31 20:42:05 +0530

    Daemonize celery

--- startcelery.sh
+++ startcelery.sh
@@ -1,6 +1,6 @@
 #python manage.py celery worker -Q wizcard --loglevel=debug
 #python manage.py celery worker --loglevel=info
-celery -A wizcard worker --loglevel=info -B
+celery -A wizcard worker --loglevel=info -B -D
 #rabbitmqctl add_user ocr_user ocr$
 #rabbitmqctl add_vhost ocr$
 #rabbitmqctl set_permissions -p ocr ocr_user ".*" ".*" ".*"$
