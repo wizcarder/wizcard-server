dbe198cc1391d4c4d076a502946f86978172696c
Anand Ramani <anandramani98@gmail.com>
2015-04-16 18:58:31 +0530

    Global aws host list

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,18 +1,13 @@
-from __future__ import absolute_import
-# ^^^ The above is required if you want to import from the celery
-# library. If you don't have this then `from celery.schedules import
-# becomes `proj.celery.schedules` in Python 2.x since it allows
-# for relative imports by default.
-# Celery settings
-
-
+AWSHOSTS=['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com','ec2-52-74-60-152.ap-southeast-1.compute.amazonaws.com']
 ALLHOSTS = {
 	'test':{
 		'LOCATIONSERVER': ['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com'],
-		'WIZSERVER':['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com','ec2-52-74-60-152.ap-southeast-1.compute.amazonaws.com'],
+		'WIZSERVER':AWSHOSTS,
+                'NGINX':AWSHOSTS
 	},
         'dev':{
             'LOCATIONSERVER':['localhost'],
             'WIZSERVER':['localhost'],
+            'NGINX':['localhost']
         }
 }
