88e86e8172be45463de7d88b6c62d4570dbcc641
Anand Ramani <anandramani98@gmail.com>
2015-04-16 14:16:59 +0530

    Read from instances and env

--- location_service/rconfig.py
+++ location_service/rconfig.py
@@ -1,6 +1,11 @@
+import sys
+sys.path.append(".")
+import os
+from wizcard import instances
+RUNENV=os.getenv("WIZRUNENV", "dev")
 EXCHANGE = 'trees'
 EXCHANGE_TYPE = 'direct'
 QUEUE = 'treeQ'
 ROUTING_KEY = 'location_service'
-HOST = 'localhost'
+HOST = instances.ALLHOSTS[RUNENV]['LOCATIONSERVER'][0]
 
