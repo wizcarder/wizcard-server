1f4d44e46518478a1d84a00e2a68dce86ecf20e7
Anand Ramani <anandramani98@gmail.com>
2015-04-21 13:02:01 +0530

    Instances - Host dictionaries and environments defined - SOT for all instances used

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python
 AWSHOSTS=['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com','ec2-52-74-60-152.ap-southeast-1.compute.amazonaws.com']
+
+memcache_hosts=[]
+for hosts in AWSHOSTS:
+    memcache_hosts.append(hosts+":11211")
 ALLHOSTS = {
 	'test':{
 		'LOCATIONSERVER': ['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com'],
 		'WIZSERVER':AWSHOSTS,
-                'NGINX':AWSHOSTS
+                'NGINX':AWSHOSTS,
+                'MEMCACHE':memcache_hosts,
 	},
         'dev':{
             'LOCATIONSERVER':['localhost'],
             'WIZSERVER':['localhost'],
             'NGINX':['localhost']
         }
+
 }
+
