65e04d88d00c2b9a8f38187a29945c33864c3442
Anand Ramani <anandramani98@gmail.com>
2015-04-21 15:24:30 +0530

    Updated new EC2 instances

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-AWSHOSTS=['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com','ec2-52-74-60-152.ap-southeast-1.compute.amazonaws.com']
+AWSHOSTS=['ec2-52-74-114-151.ap-southeast-1.compute.amazonaws.com','ec2-52-74-148-23.ap-southeast-1.compute.amazonaws.com']
 
 memcache_hosts=[]
 for hosts in AWSHOSTS:
     memcache_hosts.append(hosts+":11211")
 ALLHOSTS = {
 	'test':{
-		'LOCATIONSERVER': ['ec2-52-74-128-1.ap-southeast-1.compute.amazonaws.com'],
+		'LOCATIONSERVER': ['ec2-52-74-114-151.ap-southeast-1.compute.amazonaws.com'],
 		'WIZSERVER':AWSHOSTS,
                 'NGINX':AWSHOSTS,
                 'MEMCACHE':memcache_hosts,
