7fc970dfd93c18d6d84e76e4eb649677d649a512
Anand Ramani <anandramani98@gmail.com>
2015-05-03 20:26:12 +0530

    Configured to us-west

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-AWSHOSTS=['ec2-52-74-114-151.ap-southeast-1.compute.amazonaws.com','ec2-52-74-148-23.ap-southeast-1.compute.amazonaws.com']
+AWSHOSTS=['ec2-52-8-25-4.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com']
 
 memcache_hosts=[]
 for hosts in AWSHOSTS:
     memcache_hosts.append(hosts+":11211")
 ALLHOSTS = {
 	'test':{
-		'LOCATIONSERVER': ['ec2-52-74-114-151.ap-southeast-1.compute.amazonaws.com'],
+		'LOCATIONSERVER': ['ec2-52-8-25-4.us-west-1.compute.amazonaws.com'],
 		'WIZSERVER':AWSHOSTS,
                 'NGINX':AWSHOSTS,
                 'MEMCACHE':memcache_hosts,
