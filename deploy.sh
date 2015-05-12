bbef25ffb32ec3ffb558970929c4fc1dcce70e73
Anand Ramani <anandramani98@gmail.com>
2015-05-12 20:49:13 +0530

    parameter based

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-fab -f upstart_fabfile.py   --set function=LOCATIONSERVER,henv=test set_hosts deploy -i ~/aws/wiztest.pem -u ubuntu
-fab -f upstart_fabfile.py   --set function=WIZSERVER,henv=test set_hosts deploy -i ~/aws/wiztest.pem -u ubuntu
+fab  --set function=LOCATIONSERVER,henv=test set_hosts $1 -i ~/aws/stagewizcard.pem -u ubuntu
+fab --set function=WIZSERVER,henv=test set_hosts $1 -i ~/aws/stagewizcard.pem -u ubuntu
 
