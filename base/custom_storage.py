fa00a0c86b28e1cd44e1513fec22932966b3e236
Ubuntu <ubuntu@ip-172-31-19-237.us-west-1.compute.internal>
2015-05-17 18:26:15 +0000

    Fixed boto issues with migrations

--- base/custom_storage.py
+++ base/custom_storage.py
@@ -1,5 +1,7 @@
 from queued_storage.backends import QueuedS3BotoStorage
+from django.utils.deconstruct import deconstructible
 
+@deconstructible
 class WizcardQueuedS3BotoStorage(QueuedS3BotoStorage):
     def __init__(self, *args, **kwargs):
         super(WizcardQueuedS3BotoStorage, self).__init__(*args, **kwargs)
