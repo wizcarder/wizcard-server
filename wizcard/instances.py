3510394419b5c437638b09d0abd959db41a4b256
Anand Ramani <anandramani98@gmail.com>
2015-05-18 17:30:24 +0530

    Added dev hosts

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 STG_AWSHOSTS=['ec2-52-8-25-4.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com']
 PROD_AWSHOSTS=['ec2-52-8-96-234.us-west-1.compute.amazonaws.com','ec2-52-8-29-117.us-west-1.compute.amazonaws.com']
+DEV_AWSHOSTS=['wizdev.aws.com']
 
 stg_memcache_hosts=[]
 prod_memcache_hosts=[]
 for hosts in STG_AWSHOSTS:
     stg_memcache_hosts.append(hosts+":11211")
 for hosts in PROD_AWSHOSTS:
+    prod_memcache_hosts.append(hosts+":11211")
+for hosts in DEV_AWSHOSTS:
     prod_memcache_hosts.append(hosts+":11211")
 ALLHOSTS = {
 	'test':{
@@ -16,9 +19,10 @@ ALLHOSTS = {
                 'MEMCACHE':stg_memcache_hosts,
 	},
         'dev':{
-            'LOCATIONSERVER':['localhost'],
-            'WIZSERVER':['localhost'],
-            'NGINX':['localhost']
+            'LOCATIONSERVER':DEV_AWSHOSTS,
+            'WIZSERVER':DEV_AWSHOSTS,
+            'NGINX':DEV_AWSHOSTS,
+            'MEMCACHE':DEV_AWSHOSTS,
         },
         'prod':{
             'LOCATIONSERVER':[PROD_AWSHOSTS[0]],
