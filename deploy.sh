938055d0743bf3aea41d7b79e3584c6bba3e922e
Anand Ramani <anandramani98@gmail.com>
2015-05-18 20:34:19 +0530

    more parameters

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-fab  --set function=LOCATIONSERVER,henv=$2 set_hosts $1 -i ~/aws/stagewizcard.pem -u ubuntu
-fab --set function=WIZSERVER,henv=$2 set_hosts $1 -i ~/aws/stagewizcard.pem -u ubuntu
+fab  --set function=LOCATIONSERVER,henv=$2,runuser=$3 set_hosts $1 -i ~/aws/stagewizcard.pem -u $3
+fab --set function=WIZSERVER,henv=$2,runuser=$3 set_hosts $1 -i ~/aws/stagewizcard.pem -u $3
 
