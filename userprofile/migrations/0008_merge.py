052149dbd6f77ef19907b64cc8615d32200fdf22
Ubuntu <ubuntu@ip-172-31-18-177.us-west-1.compute.internal>
2015-05-27 16:58:19 +0000

    merge conflict

---
+++ userprofile/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('userprofile', '0007_merge'),
+        ('userprofile', '0006_auto_20150525_1434'),
+    ]
+
+    operations = [
+    ]
