f480f9b793ecdb3a94bcc70f08a0bae5b210e91f
Anand Ramani <anandramani98@gmail.com>
2015-06-21 23:58:24 +0530

    Changed AWS S3 Bucket env

--- wizcard/settings.py
+++ wizcard/settings.py
@@ -336,12 +336,8 @@ INSTALLED_APPS = (
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = 'AKIAJ7JLJSP4BCEZ72EQ'
 AWS_SECRET_ACCESS_KEY = '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'
-if RUNENV == 'stage' or RUNENV == 'dev':
-    AWS_TEST_BUCKET = '-test'
-elif RUNENV == 'prod':
-    AWS_TEST_BUCKET = '-prod'
-
-AWS_STORAGE_BUCKET_NAME = 'wizcard-image-bucket' + AWS_TEST_BUCKET
+AWS_BUCKET_ENV = "-" + RUNENV
+AWS_STORAGE_BUCKET_NAME = 'wizcard-image-bucket' + AWS_BUCKET_ENV
 S3_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_DIRECTORY = '/static/'
 MEDIA_DIRECTORY = '/media/'
