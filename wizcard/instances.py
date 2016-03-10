269bae6c5f526d1d8e936cb46c73ea4ef1aceec4
Anand Ramani <anandramani98@gmail.com>
2016-03-10 20:15:10 +0530

    Added one more staging node

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-#STG_AWSHOSTS=['ec2-52-8-141-105.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com']
-STG_AWSHOSTS=['ec2-54-183-239-37.us-west-1.compute.amazonaws.com']
+STG_AWSHOSTS=['ec2-52-8-141-105.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com','ec2-54-183-239-37.us-west-1.compute.amazonaws.com']
 PROD_AWSHOSTS=['ec2-52-8-96-234.us-west-1.compute.amazonaws.com','ec2-52-8-29-117.us-west-1.compute.amazonaws.com']
 DEV_AWSHOSTS=['localhost']
 
@@ -14,7 +13,7 @@ for hosts in DEV_AWSHOSTS:
     prod_memcache_hosts.append(hosts+":11211")
 ALLHOSTS = {
 	'stage':{
-		'LOCATIONSERVER': [STG_AWSHOSTS[0]],
+		'LOCATIONSERVER': [STG_AWSHOSTS[0], STG_AWSHOSTS[2]],
 		'WIZSERVER':STG_AWSHOSTS,
                 'NGINX':STG_AWSHOSTS,
                 'MEMCACHE':stg_memcache_hosts,
