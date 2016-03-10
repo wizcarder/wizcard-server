a4a1c6caa09dce533e747993fac0775d160e6e56
Anand Ramani <anandramani98@gmail.com>
2016-03-10 23:09:31 +0530

    Changed instances for launch tomm

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-STG_AWSHOSTS=['ec2-52-8-141-105.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com','ec2-54-183-239-37.us-west-1.compute.amazonaws.com']
+DEV_AWSHOSTS=['ec2-54-183-239-37.us-west-1.compute.amazonaws.com']
+STG_AWSHOSTS=['ec2-52-8-141-105.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com']
 PROD_AWSHOSTS=['ec2-52-8-96-234.us-west-1.compute.amazonaws.com','ec2-52-8-29-117.us-west-1.compute.amazonaws.com']
-DEV_AWSHOSTS=['localhost']
+#DEV_AWSHOSTS=['localhost']
 
 stg_memcache_hosts=[]
 prod_memcache_hosts=[]
@@ -13,7 +14,7 @@ for hosts in DEV_AWSHOSTS:
     prod_memcache_hosts.append(hosts+":11211")
 ALLHOSTS = {
 	'stage':{
-		'LOCATIONSERVER': [STG_AWSHOSTS[0], STG_AWSHOSTS[2]],
+		'LOCATIONSERVER': [STG_AWSHOSTS[0]],
 		'WIZSERVER':STG_AWSHOSTS,
                 'NGINX':STG_AWSHOSTS,
                 'MEMCACHE':stg_memcache_hosts,
