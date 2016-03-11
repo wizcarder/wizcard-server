a9a3ad02dec45314fe79dfd5b73eb3fb63b17308
Anand Ramani <anandramani98@gmail.com>
2016-03-11 23:22:47 +0530

    Phone check disabled for staging and production

--- wizcard/settings.py
+++ wizcard/settings.py
@@ -250,7 +250,7 @@ MAX_PHONE_CHECK_RETRIES = 3
 #for UT..avoid nexmo
 PHONE_CHECK = False
 if RUNENV == "stage" or RUNENV == "prod":
-    PHONE_CHECK = True
+    PHONE_CHECK = False
 #retry timeout
 PHONE_CHECK_TIMEOUT = 180
 
