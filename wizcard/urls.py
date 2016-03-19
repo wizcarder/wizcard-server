91c2a1fffea6d608cff127be54d96fc5d3d84bc4
Anand R <anandramani98@gmail.com>
2016-03-19 07:56:43 +0530

    Added healthcheck for django and DB

--- wizcard/urls.py
+++ wizcard/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wizcard_schema.views.home', name='home'),
     url(r'^$', include('wizserver.urls')),
+    url(r'^healthstatus$', include('healthstatus.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
