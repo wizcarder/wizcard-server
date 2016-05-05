1102dd7baee014a28b8142dfbb47dd88067e4376
Anand Ramani <anandramani98@gmail.com>
2016-05-05 10:00:38 +0530

    Changed template and coordinates moved template to settings

--- lib/emailInvite.py
+++ lib/emailInvite.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import absolute_import
 import sys
 import StringIO, hashlib
 from celery import shared_task
@@ -6,28 +7,30 @@ from django.utils import timezone
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.files.storage import default_storage as storage
 from wizcardship.models import WizcardManager, Wizcard
+from wizcard import settings
 from PIL import Image,ImageFont, ImageDraw
 import pdb
 now = timezone.now
 
+
 @shared_task
 def create_template(wizcard_id):
 
-    resource = storage.open("/invites/email_template.png")
+    resource = storage.open(settings.EMAIL_TEMPLATE)
 
     wizcard = Wizcard.objects.get(id=wizcard_id)
 
     data = {"name" : wizcard.first_name + " " + wizcard.last_name, "company": wizcard.get_latest_company(), "title" : wizcard.get_latest_title(), "email" : wizcard.email, "phone" : wizcard.phone}
 
-    position = {'email': '400,440', 'title': '382,400', 'phone': '194,440', 'name':'382,307', 'company' : '382, 373'}
+    position = {'email': '490,462', 'title': '380,388', 'phone': '198,464', 'name':'378,315', 'company' : '381, 371'}
     im = Image.open(resource)
     im_sz = im.size
     im_bg = Image.new(mode='RGBA', size=im_sz, color=(255, 255, 255, 230))
     im_bg.paste(im, (0, 0), 0)
-    defaultfont = ImageFont.truetype('arial.ttf', 15)
+    defaultfont = ImageFont.truetype('Roboto-Regular.ttf', 18)
 
     try:
-        namefont = ImageFont.truetype('Arial Bold.ttf', 20)
+        namefont = ImageFont.truetype('Roboto-Bold.ttf', 20)
     except:
         namefont = ImageFont.truetype('Arial_Bold.ttf', 20)
 
