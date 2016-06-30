18726728095d0876fa83ee4a92253be6f87c6b4b
Anand Ramani <anandramani98@gmail.com>
2016-06-30 06:22:31 +0000

    Fixed GCM from data to notification

--- notifications/androidgcm.py
+++ notifications/androidgcm.py
@@ -10,7 +10,7 @@ def send_gcm_message(key,reg_token,data):
 
 
 
-    response = gcm.json_request(registration_ids=reg_token, data=data,delay_while_idle=False)
+    response = gcm.json_request(registration_ids=reg_token, notification=data,delay_while_idle=False)
 
     # Successfully handled registration_ids
     if response and 'success' in response:
