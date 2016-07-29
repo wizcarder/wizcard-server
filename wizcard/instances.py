3180f6870f7fb3611aa9dc02e63416e7775303fa
Anand Ramani <anandramani98@gmail.com>
2016-07-29 01:15:31 +0000

    Added prod instances

--- wizcard/instances.py
+++ wizcard/instances.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 TEST_AWSHOSTS=['ec2-52-66-102-242.ap-south-1.compute.amazonaws.com']
 DEV_AWSHOSTS=['ec2-54-153-11-241.us-west-1.compute.amazonaws.com']
-PROD_AWSHOSTS=['ec2-54-193-120-172.us-west-1.compute.amazonaws.com','ec2-52-8-23-147.us-west-1.compute.amazonaws.com']
+PROD_AWSHOSTS=['ec2-52-66-114-251.ap-south-1.compute.amazonaws.com','ec2-52-66-110-201.ap-south-1.compute.amazonaws.com']
 STG_AWSHOSTS=['localhost']
 
 stg_memcache_hosts=[]
