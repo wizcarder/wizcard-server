1111db3d4a56b1bfd048c04daa1c3e2b7a0735f4
Anand Ramani <anandramani98@gmail.com>
2016-10-02 20:05:18 +0530

    recommendation/migrations/0001_initial.py

--- recommendation/migrations/0001_initial.py
+++ recommendation/migrations/0001_initial.py
@@ -14,28 +14,28 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Recommendation_a1',
+            name='Recommendation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('reco_object_id', models.PositiveIntegerField()),
                 ('score', models.DecimalField(max_digits=5, decimal_places=2)),
                 ('recomodel', models.IntegerField(choices=[(0, b'AB_RECO'), (1, b'WIZCONNECTIONS_RECO')])),
-                ('reco_content_type', models.ForeignKey(related_name='reco_a1', to='contenttypes.ContentType')),
+                ('reco_content_type', models.ForeignKey(related_name='reco', to='contenttypes.ContentType')),
             ],
         ),
         migrations.CreateModel(
-            name='UserRecommendation_a1',
+            name='UserRecommendation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('useraction', models.PositiveSmallIntegerField(default=3, choices=[(0, b'VIEWED'), (1, b'ACTED'), (2, b'DISMISSED'), (3, b'NEW')])),
-                ('reco', models.ForeignKey(to='recommendation_alt1.Recommendation_a1')),
+                ('reco', models.ForeignKey(to='recommendation.Recommendation')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
-            model_name='recommendation_a1',
+            model_name='recommendation',
             name='recommendation_for',
-            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, through='recommendation_alt1.UserRecommendation_a1'),
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, through='recommendation.UserRecommendation'),
         ),
     ]
