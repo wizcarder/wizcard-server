11821801ff3fee12cc5ed3eeabffed56481022e6
Anand Ammundi <aammundi@gmail.com>
2016-11-03 18:49:48 +0530

    notes backward compat issue during resync

--- userprofile/models.py
+++ userprofile/models.py
@@ -251,7 +251,7 @@ class UserProfile(models.Model):
                 asset_id=x.cctx.asset_id,
                 asset_type=x.cctx.asset_type,
                 connection_mode=x.cctx.connection_mode,
-                notes=x.cctx.notes if x.cctx.notes else "",
+                notes=x.cctx.notes if hasattr(x.cctx, 'notes') else "",
                 timestamp=x.created.strftime("%d %B %Y")).context, conn)
 
             s['context'] = serialize(cctx, **fields.cctx_wizcard_template)
