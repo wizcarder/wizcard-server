20a603188dbb0e8fda896bf5609f190599c18caf
Anand Ramani <anandramani98@gmail.com>
2016-11-09 00:12:37 +0530

    Covered for multiple result types in gcm

--- notifications/androidgcm.py
+++ notifications/androidgcm.py
@@ -15,8 +15,15 @@ def send_gcm_message(key, reg_token, data):
 	message_title = data['title']
 	message_body = data['body']
 	result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+
+	if type(result) is list:
+		push_result = result[0]['success']
+	elif type(result) is dict:
+		push_result = result['success']
+	else:
+		push_result = -1
 	
-	if result['success'] > 0:
+	if push_result > 0:
 		logger.debug("Successfully sent notification for %s",reg_token)
 	else:
 		logger.debug("Remove reg_token %s", reg_token)
