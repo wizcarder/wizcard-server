1efa25c9e5090c69fea2e6684da75c8a5790638c
Anand Ramani <anandramani98@gmail.com>
2016-11-08 18:29:49 +0530

    Removed twistd and tweaked memcached status

--- upstart/check_process.sh
+++ upstart/check_process.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for i in wizserver celeryworker memcached twistd celeryflower celerybeat locationjob
+for i in wizserver celeryworker celeryflower celerybeat locationjob
  do
   sudo service $i status
  done
+
+sudo /etc/init.d/memcached/status
 
