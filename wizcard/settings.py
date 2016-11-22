fc90533146680f9c31999a20b8f56f12933d0f51
Anand Ramani <anandramani98@gmail.com>
2016-11-22 06:51:40 +0000

    Fixed path in email default image

--- wizcard/settings.py
+++ wizcard/settings.py
@@ -298,7 +298,7 @@ AWS_SECRET_ACCESS_KEY = '23wDEZPCxXTs0zVnxcznzDsoDzm4KWo0NMimWe+0'
 AWS_BUCKET_ENV = "-" + RUNENV
 AWS_STORAGE_BUCKET_NAME = 'wizcard-image-bucket' + AWS_BUCKET_ENV
 S3_URL = 'http://s3.us-west-1.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
-EMAIL_DEFAULT_IMAGE = S3_URL +  "invites/email_info.png"
+EMAIL_DEFAULT_IMAGE = S3_URL +  "/invites/email_info.png"
 
 STATIC_DIRECTORY = '/static/'
 MEDIA_DIRECTORY = '/media/'
