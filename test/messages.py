ebca6dd57d9a5cf7553c32792b1c85b8ae38da8e
Anand Ramani <anandramani98@gmail.com>
2016-11-29 23:45:20 +0530

    Added messages for self invite

--- test/messages.py
+++ test/messages.py
@@ -34,6 +34,8 @@ FUTURE_EMAIL4 = "mnop@future.com"
 OCR_PHONE = "+919590203441"
 OCR_USERNAME = OCR_PHONE+'@wizcard.com'
 
+SELF_PHONE = "+" + NEXMO_PHONE1
+
 
 EMAIL1 = "aammundi@gmail.com"
 EMAIL2 = "amsaha@gmail.com"
