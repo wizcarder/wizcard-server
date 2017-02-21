35c2c3343b1434596aaad5bf8a580afbdfb67261
Ubuntu <ubuntu@ip-172-31-20-255.ap-south-1.compute.internal>
2017-02-21 09:50:35 +0000

    Migrations for wizcard

---
+++ wizcardship/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wizcardship', '0019_merge'),
+        ('wizcardship', '0015_auto_20161230_2305'),
+    ]
+
+    operations = [
+    ]
