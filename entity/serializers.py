0c1e7279d1b9b42065150e5112542d31981d9e70
Ubuntu <ubuntu@ip-172-31-2-49.ap-south-1.compute.internal>
2017-03-31 14:50:42 +0000

    phone and email update and create bug fix

--- entity/serializers.py
+++ entity/serializers.py
@@ -92,8 +92,6 @@ class EntitySerializer(serializers.ModelSerializer):
         owners = validated_data.pop('owners', None)
         sub_entities = validated_data.pop('related', None)
         location = validated_data.pop('location', None)
-        phone = validated_data.pop('phone', None)
-        email = validated_data.pop('email', None)
         entity_type = validated_data.pop('entity_type')
 
         cls, ser = BaseEntity.get_entity_from_type(entity_type)
@@ -116,8 +114,6 @@ class EntitySerializer(serializers.ModelSerializer):
         instance.name = validated_data.pop('name', instance.name)
         instance.address = validated_data.pop('address', instance.address)
         instance.website = validated_data.pop('website', instance.website)
-        instance.phone = validated_data.pop('phone', instance.phone)
-        instance.email = validated_data.pop('phone', instance.email)
         instance.description = validated_data.pop('description', instance.description)
 
         # handle related objects. It's a replace
