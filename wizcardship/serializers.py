1ddea41f83bf3dccb136a87a78dcedb506a52f1e
Anand Ammundi <aammundi@gmail.com>
2017-03-31 12:59:36 +0530

    Event Modelling End Points Added event, prod, biz end-points

--- wizcardship/serializers.py
+++ wizcardship/serializers.py
@@ -1,43 +1,7 @@
 __author__ = 'aammundi'
 from rest_framework import serializers
-from media_mgr.serializers import MediaObjectsSerializer
-from rest_framework.validators import ValidationError
-from userprofile.models import UserProfile
 from wizcardship.models import Wizcard
 
-
-
-class RelatedSerializerField(serializers.RelatedField):
-
-    def get_queryset(self):
-        pass
-
-    def to_internal_value(self, data):
-        id = data.get('id', None)
-        type = data.get('type', None)
-
-        # Perform the data validation.
-        if not id:
-            raise ValidationError({
-                'id': 'This field is required.'
-            })
-        if not type:
-            raise ValidationError({
-                'type': 'This field is required.'
-            })
-
-        return {
-            'id': int(id),
-            'type': type
-        }
-
-    def to_representation(self, value):
-        obj_id = value.object.id
-        type = value.alias
-        return 'id: %d, type: %s' % (obj_id, type)
-
-
-   
 
 class WizcardSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -45,66 +9,9 @@ class WizcardSerializer(serializers.ModelSerializer):
 
     def get_extFields(self, obj):
         return obj.get_extFields
-    
 
     class Meta:
         model = Wizcard
-        fields = ('pk','user','first_name','last_name', 'phone', 'email','thumbnailImage', 'videoUrl', 'videoThumbnailUrl', 'extFields',
-                'smsurl', 'vcard')
-
-
-
-'''
-    def create(self, validated_data):
-        media = validated_data.pop('media', None)
-        tags = validated_data.pop('tags', None)
-        owners = validated_data.pop('owners', None)
-        sub_entities = validated_data.pop('related', None)
-        location = validated_data.pop('location', None)
-
-        event = Event.objects.create(**validated_data)
-        if media:
-            media_create.send(sender=event, objs=media)
-        if owners:
-            for o in owners:
-                event.add_owner(o)
-        if sub_entities:
-            for s in sub_entities:
-                event.add_subentity_by_id(**s)
-        if location:
-            event.create_or_update_location(location['lat'], location['lng'])
-
-        return event
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.pop('name', instance.name)
-        instance.address = validated_data.pop('address', instance.address)
-        instance.website = validated_data.pop('website', instance.website)
-        instance.description = validated_data.pop('description', instance.description)
-
-        # handle related objects. It's a replace
-        media = validated_data.pop('media', None)
-        if media:
-            instance.media.all().delete()
-            media_create.send(sender=instance, objs=media)
-
-        owners = validated_data.pop('owners', None)
-        if owners:
-            instance.owners.clear()
-            for o in owners:
-                instance.add_owner(o)
-
-        sub_entities = validated_data.pop('related', None)
-        if sub_entities:
-            instance.related.all().delete()
-            for s in sub_entities:
-                instance.add_subentity_by_id(**s)
-
-        location = validated_data.pop('location', None)
-        if location:
-            instance.create_or_update_location(location['lat'], location['lng'])
-
-        instance.save()
-        return instance
-'''
+        fields = ('pk','user', 'first_name', 'last_name', 'phone', 'email', 'thumbnailImage', 'videoUrl', 'videoThumbnailUrl', 'extFields',
+                  'smsurl', 'vcard')
 
