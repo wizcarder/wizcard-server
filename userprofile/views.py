26f87fc29989d7ef8557ae9f2d9ee1055ba00247
Anand Ramani <anandramani98@gmail.com>
2017-04-06 15:23:13 +0530

    Changed everything to User (given post_save handles userprofile)

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,29 +1,20 @@
 # Create your views here.
-from  userprofile.models import UserProfile
-from rest_framework import viewsets, serializers
+from userprofile.serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 import django_filters
-
 import pdb
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = (['id', 'userid'])
 
 class UserQueryFilter(django_filters.rest_framework.FilterSet):
-    email = django_filters.CharFilter(name="user__email")
-    username = django_filters.CharFilter(name="user__username")
+    email = django_filters.CharFilter(name="email")
+    username = django_filters.CharFilter(name="username")
 
     class Meta:
-        model = UserProfile
-        fields = ['email', 'username']
+        model = User
+        fields = ['id','email', 'username']
 
-
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = UserProfile.objects.all()
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_class = UserQueryFilter
-
-
-
-
