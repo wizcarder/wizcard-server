ea18deaf7bcfa8ca9ec9d968d17d28cc73548d71
Anand Ramani <anandramani98@gmail.com>
2017-04-06 15:23:54 +0530

    Serializer for User handling create and retrieve on POST

---
+++ userprofile/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import viewsets, serializers
+from django.contrib.auth.models import User
+from userprofile.models import UserProfile
+from rest_framework.response import Response
+import hashlib
+import pdb
+
+class UserSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = User
+        fields = (['id', 'email'])
+
+
+    def create(self, validated_data):
+        email = validated_data.get('email',None)
+        if email:
+            user, created = User.objects.get_or_create(username=email, email=email)
+            if created:
+                user.set_password(hashlib.sha1(email).hexdigest())
+                user.save()
+                user.profile.future_user = True
+                user.profile.save()
+            return user
+        else:
+            return Response
+
+
