83c86c9be407b95bf4e57422738ef6435a1d65c5
Anand Ammundi <aammundi@gmail.com>
2017-05-26 13:20:34 +0530

    Added DRF WizCard serialization for App

--- wizcardship/serializers.py
+++ wizcardship/serializers.py
@@ -1,25 +1,47 @@
 __author__ = 'aammundi'
 from rest_framework import serializers
-from wizcardship.models import Wizcard
+from wizcardship.models import Wizcard, ContactContainer
 from media_mgr.serializers import MediaObjectsSerializer
 
+
+class ContactContainerSerializerL0(serializers.ModelSerializer):
+    f_card_url = serializers.URLField(source='get_fbizcard_url')
+
+    class Meta:
+        model = ContactContainer
+        fields = ('id', 'title', 'company', 'f_card_url')
+
+
+class ContactContainerSerializerL1(ContactContainerSerializerL0):
+    pass
+
+
 class WizcardSerializerThumbnail(serializers.ModelSerializer):
+    wizcard_id = serializers.IntegerField(source='id')
+
     class Meta:
         model = Wizcard
-        fields = ('id', 'thumbnail',)
+        fields = ('wizcard_id', 'thumbnail_url',)
 
-    thumbnail = serializers.URLField(source='get_thumbnail_url')
+    thumbnail_url = serializers.URLField(source='get_thumbnail_url')
 
 
 class WizcardSerializerL1(WizcardSerializerThumbnail):
+    # AA: couldn't find a prescribed way to do this
+    user_id = serializers.PrimaryKeyRelatedField(read_only=True, source='user.id')
+    contact_container = ContactContainerSerializerL0(many=True, read_only=True)
     media = MediaObjectsSerializer(many=True)
 
     class Meta(WizcardSerializerThumbnail.Meta):
         model = Wizcard
-        l1_fields = ('first_name', 'last_name', 'phone', 'email', 'media', 'user')
+        l1_fields = ('first_name', 'last_name', 'phone', 'email', 'user_id', 'contact_container')
         fields = WizcardSerializerThumbnail.Meta.fields + l1_fields
 
 
+class WizcardSerializerL2(WizcardSerializerL1):
+    pass
+
+# not used by App
 class WizcardSerializer(WizcardSerializerL1):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
     extFields = serializers.DictField()
