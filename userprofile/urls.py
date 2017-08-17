ecf31acda1f8615755accdf446c9f0d2b6ccb115
Anand Ramani <anandramani98@gmail.com>
2017-08-17 11:36:27 +0530

    Login added more details and also used inbuilt userview instead of custom method (ProfileView)

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,8 +1,8 @@
 __author__ = 'aammundi'
-from userprofile.views import UserViewSet, ProfileView
+from userprofile.views import UserViewSet, ProfileView, CustomObtainAuthToken
 from rest_framework.routers import DefaultRouter
 from django.conf.urls import patterns, include, url
-from rest_auth.views import LoginView, LogoutView
+from rest_auth.views import LoginView, LogoutView, UserDetailsView
 
 
 router = DefaultRouter()
@@ -14,9 +14,10 @@ urlpatterns = [
 
 #    url(r'^account-confirm-email/(?P<key>\w+)/$', allauthemailconfirmation, name="account_confirm_email"),
     url(r'^registration/', include('rest_auth.registration.urls')),
-    url(r'^login/$', LoginView.as_view(), name='rest_login'),
+    url(r'^login/$', CustomObtainAuthToken.as_view(), name='user_login'),
     url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
-    url(r'^profile/$', ProfileView.as_view(), name='profile')
+    #url(r'^profile/$', ProfileView.as_view(), name='profile')
+    url(r'^profile/$', UserDetailsView.as_view(), name='profile' )
 #    url(r'^password/', LogoutView.as_view(), name='rest_logout'),
 #    url(r'^logout/', include('rest_auth.urls')),
 #   # url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login')
--- userprofile/views.py
+++ userprofile/views.py
@@ -3,9 +3,12 @@ from userprofile.serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework import status
 import django_filters
-
+import pdb
 
 class UserQueryFilter(django_filters.rest_framework.FilterSet):
     email = django_filters.CharFilter(name="email")
@@ -25,5 +28,18 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+
+        try:
+            response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+            token = Token.objects.get(key=response.data['token'])
+            user = User.objects.get(id=token.user_id)
+            data = UserSerializer(user).data
+            return Response({'token': token.key, 'user': data})
+        except:
+            return Response({'error': "Could not login with credentials"}, status=status.HTTP_403_FORBIDDEN)
 
 
