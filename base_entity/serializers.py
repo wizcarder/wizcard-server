f30ef217cd2662ff22763019f78b07b83afc4c93
Anand Ramani <anandramani98@gmail.com>
2017-08-30 23:11:48 +0530

    Removed self user/wizcard from attendee list

--- base_entity/serializers.py
+++ base_entity/serializers.py
@@ -176,8 +176,8 @@ class EntitySerializerL2(TaggitSerializer, EntitySerializerL1):
         if not count:
             return out
 
-        wizcards = map(lambda u: u.wizcard, obj.users.exclude(wizcard__isnull=True))
         user = self.context.get('user', None)
+        wizcards = map(lambda u: u.wizcard, obj.users.exclude(wizcard__isnull=True, id=user.id))
 
         out = dict(
             count=count,
