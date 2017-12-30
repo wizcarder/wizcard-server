b875b3854c7dcf8a1f958fe359db4d1bc8f3f7b6
Anand Ammundi <aammundi@gmail.com>
2017-12-30 10:07:28 +0530

    missed out changes and pycharm warning fixes

--- taganomy/models.py
+++ taganomy/models.py
@@ -13,7 +13,14 @@ import pdb
 
 # Its a Taxanomy + tags => Taganomy
 
+
 class TaganomyManager(BaseEntityComponentManager):
+
+    def owners_entities(self, user, entity_type=BaseEntityComponent.CATEGORY):
+        return super(TaganomyManager, self).owners_entities(
+            user,
+            entity_type=entity_type
+        )
 
     def get_category(self, tags):
         cats = self.filter(tags__name__in=[tags])
@@ -29,21 +36,20 @@ class Taganomy(BaseEntityComponent):
     def delete_related_tags(self, tags):
         related = self.get_parent_entities()
         remove_tags = Tag.objects.filter(id__in=tags)
-        map(lambda x:x.tags.remove(*remove_tags), related)
+        map(lambda x: x.tags.remove(*remove_tags), related)
 
-
-from django.db.models.signals import m2m_changed
 
 def tag_signal_handler(sender, **kwargs):
 
     instance = kwargs.pop("instance", None)
     action = kwargs.pop("action", None)
-    tag_ids = kwargs.pop("pk_set",None)
+    tag_ids = kwargs.pop("pk_set", [None])
 
     if action == "post_remove" and \
             ContentType.objects.get_for_model(instance) == ContentType.objects.get(model="taganomy"):
         instance.delete_related_tags(list(tag_ids))
     else:
         return
+
 
 m2m_changed.connect(tag_signal_handler, sender=Taganomy.tags.through)
--- taganomy/serializers.py
+++ taganomy/serializers.py
@@ -1,9 +1,7 @@
 
 from rest_framework import serializers
 from taganomy.models import Taganomy
-from django.contrib.auth.models import User
-from taggit_serializer.serializers import (TagListSerializerField,
-                                           TaggitSerializer)
+from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
 from base_entity.serializers import EntitySerializer
 from base_entity.models import BaseEntityComponent
 import pdb
@@ -15,15 +13,13 @@ class TaganomySerializer(EntitySerializer, TaggitSerializer):
 
     class Meta:
         model = Taganomy
-        fields = ('id', 'tags', 'category')
-
+        fields = ('id', 'tags', 'category',)
 
     def prepare(self, validated_data):
         super(TaganomySerializer, self).prepare(validated_data)
 
     def post_create(self, entity):
         super(TaganomySerializer, self).post_create(entity)
-
 
     def create(self, validated_data, **kwargs):
         validated_data.update(entity_type=BaseEntityComponent.CATEGORY)
@@ -38,6 +34,3 @@ class TaganomySerializer(EntitySerializer, TaggitSerializer):
         instance.category = validated_data.pop('category', instance.category)
         tags = validated_data.pop('category', instance.tags)
         instance.tags.set(*tags)
-
-
-
