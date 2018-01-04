5c13ac47063a8115cf95e45194afa99e2a961370
Anand Ammundi <aammundi@gmail.com>
2018-01-04 10:23:04 +0530

    merge revert of this location_server include

--- location_service/tree_state_server.py
+++ location_service/tree_state_server.py
@@ -5,8 +5,8 @@ sys.path.append(".")
 
 from lib.pytrie import SortedStringTrie as trie
 from lib import wizlib
-#from base.db import WizcardDB
-from base.rds_db import WizcardDB
+from base.db import WizcardDB
+#from base.rds_db import WizcardDB
 from rabbit_service.server import RabbitServer
 from wizcard import settings
 from rabbit_service import rconfig
